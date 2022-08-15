8282caa2287fcaf4967f166741e2703e41a2f500
Anshuman Singh <singhanshuman1020@gmail.com>
2022-08-15 21:43:28 +0530

    Taliwind configured

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 // Home component for the live view
 function Home() {
   return (
-    <div>Home</div>
+    <div className="text-3xl font-bold text-center">
+        Tailwind is Ready
+    </div>
   )
 }
 
