ea77f9ac2c90ba3d08c11d160040bebf6e6fe5a4
Anshuman Singh <singhanshuman1020@gmail.com>
2022-08-18 11:16:52 +0530

    Heatmapjs implementation

--- src/components/home_component/Heatmap.js
+++ src/components/home_component/Heatmap.js
@@ -1,7 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import h337 from "heatmap.js";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { BiRotateRight, BiRotateLeft } from 'react-icons/bi';
+import bg from "../static/bg.jpg";
 
 function Heatmap() {
+    const [rotate, setRotate] = useState(0);
+
     var config = {
         container: document.querySelector('.heatmap'),
         radius: 10,
@@ -47,7 +52,7 @@ function Heatmap() {
         });
 
         heatmapInstance.setData(data);
-    }, []);
+    }, [rotate]);
 
     const handleAddData = () => {
         let h = 320;
@@ -58,17 +63,64 @@ function Heatmap() {
             value: Math.random() * 100 // the value at datapoint(x, y)
         };
         heatmapInstance.addData(data);
-        console.log(data);
     }
 
-    return (<>
-        <div className="w-full flex justify-center">
-            <div className="heatmap bg-red-200 h-80 w-96">
-                Heatmap
+    const handleRotate = (type) => {
+        if (type === "right") {
+            if (rotate === 0 || rotate === 90 || rotate === 180)
+                setRotate((prev) => { return (prev + 90) });
+            else
+                setRotate(0);
+        }
+        else {
+            if (rotate === 0 || rotate === 90 || rotate === 180 || rotate === 270){
+                if(rotate === 0)
+                    setRotate(270);
+                else
+                    setRotate((prev) => { return (prev - 90) });
+            }
+            else
+                setRotate(0);
+        }
+    }
+
+    return (
+        <>
+            <div className="w-full flex justify-center">
+                <div className="flex flex-col">
+
+                    <TransformWrapper>
+                        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                            <React.Fragment>
+                                <div className="flex justify-evenly my-5">
+                                    <button onClick={() => zoomIn()} type="button" className="flex justify-center w-10 py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                        +
+                                    </button>
+                                    <button onClick={() => zoomOut()} type="button" className="flex justify-center w-10 py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                        -
+                                    </button>
+                                    <button onClick={() => resetTransform()} type="button" className="flex justify-center w-20 py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                        reset
+                                    </button>
+                                    <button onClick={() => handleRotate("left")} type="button" className="w-20 py-2 px-4 flex justify-center bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                        <BiRotateLeft size="20px" />
+                                    </button>
+                                    <button onClick={() => handleRotate("right")} type="button" className="w-20 py-2 px-4 flex justify-center bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                        <BiRotateRight size="20px" />
+                                    </button>
+                                </div>
+                                <TransformComponent>
+                                    <div className={`heatmap heatmapcss ${rotate === 90 ? 'right' : rotate === 180 ? 'upsideDown' : rotate === 270 ? 'left' : ''}`}>
+
+                                    </div>
+                                </TransformComponent>
+                            </React.Fragment>
+                        )}
+                    </TransformWrapper>
+                </div>
             </div>
-        </div>
-        <button className="border-2 border-slate-400 rounded-xl my-5" onClick={handleAddData}>Add Data</button>
-    </>
+            <button className="border-2 border-slate-400 rounded-xl my-5" onClick={handleAddData}>Add Data</button>
+        </>
     )
 }
 
