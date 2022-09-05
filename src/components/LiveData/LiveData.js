3de048a95f2399c9d8e91e0fa990821f2093e920
Anshuman Singh <singhanshuman1020@gmail.com>
2022-09-05 18:48:24 +0530

    dont know

--- src/components/LiveData/LiveData.js
+++ src/components/LiveData/LiveData.js
@@ -20,6 +20,47 @@ function LiveData(props) {
     useEffect(() => {
         console.log(auth);
     }, [auth]);
+    // ABOVE CODE IS ONLY USAGE DEMONSTRATION OF REDUX-TOOLKIT
+
+
+    const [liveData, setLiveData] = useState([
+        {
+            type: 'human',
+            value: '{"0":{"x": 8.714, "y": 12.637, "heading": 0.0},"2":{"x": 21.848, "y": 25.879, "heading": 0.184}}'
+        },
+        {
+            type: 'vehicle',
+            value: '{"0":{"x": 7.131, "y": 9.075, "heading": -0.443}}'
+        },
+    ]);
+
+
+    const fetchLiveData = (resource = "") => {
+        // pass resource = "vehicle" or "human" for specific data
+        let host = process.env.REACT_APP_NODE_BACKEND_URL || 'http://localhost:5000';
+        return new Promise((resolve, reject) => {
+            // refer to backend/index.js for details about the endpoint
+            axios({
+                url: `${host}/api/live`,
+                method: 'post',
+                data: {
+                    source: "mqtt",
+                    table: "",
+                    resource,
+                }
+            }).then((res) => {
+                // console.log("outer res: ", res.data);
+                resolve(res.data);
+            }).catch((err) => {
+                console.log("promise error: ", err);
+                reject(err);
+            })
+        })
+    }
+
+    // useInterval(() => {
+    //     fetchLiveData();
+    // }, 1000);
 
 
     return (
