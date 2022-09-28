65668be3265aa06e5bb81f5215d41d000d5fd066
Ayush Bawane <ayushbawane5@gmail.com>
2022-09-28 19:36:48 +0530

    area selection tab added

---
+++ src/components/analytics/AreaSelection.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+
+function AreaSelection() {
+
+  return (
+    <>
+      <div>
+        This is AreaSelection
+      </div>
+    </>
+  )
+}
+
+export default AreaSelection;
